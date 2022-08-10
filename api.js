56c3b30fd42e97e9d0fc1e9fe6b1614e7cfd230e
Kamila Silva <kamila.silva@pmweb.com.br>
2022-08-10 19:05:08 -0300

    ajustando nomes

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-let contas = [];
+let accounts = [];
 let id = 1;
 
 app.use(express.json());
@@ -12,7 +12,7 @@ app.post("/accounts", function (req, res) {
   const { name, overdraftProtection } = req.body;
   let currentBalance = res.req;
   currentBalance = 0;
-  const conta = {
+  const account = {
     id: id,
     name: name,
     overdraftProtection: overdraftProtection,
@@ -23,133 +23,129 @@ app.post("/accounts", function (req, res) {
   if (!name) return res.status(400).json();
 
   if (typeof name != "string") {
-    return res.status(400).json({ resposta: "Entrada inválida!" });
+    return res.status(400).json({ response: "Invalid Input" });
   }
 
   let { amount } = req.body;
 
   if (amount != null) {
-    conta.currentBalance += amount;
+    account.currentBalance += amount;
   }
 
-  contas.push(conta);
-
+  accounts.push(account);
   id += 1;
-  console.log(conta);
 
-  return res.status(201).json(conta);
+  return res.status(201).json(account);
 });
 
 app.post("/accounts/:id/deposit", function (req, res) {
   const { id } = req.params;
-  const conta = contas.find((conta) => conta.id == id);
+  const account = accounts.find((account) => account.id == id);
 
   const amount = req.body.amount;
-  const data = req.body.data;
-  const tipo = req.body.tipo;
+  const date = req.body.date;
+  const type = req.body.type;
 
   if (!amount) return res.status(400).json();
 
-  if (!conta) return res.status(404).json({ resposta: "conta inexistente" });
+  if (!account) return res.status(404).json({ response: "Account not exist" });
 
   if (amount != null) {
-    conta.currentBalance += amount;
-    conta.arrayDeposit.push({
-      data: data,
-      valor: amount,
-      tipo: tipo,
+    account.currentBalance += amount;
+    account.arrayDeposit.push({
+      date: date,
+      value: amount,
+      type: type,
     });
   }
 
-  return res.status(200).json(conta);
+  return res.status(200).json(account);
 });
 
 app.post("/accounts/:id/saque", function (req, res) {
   const { id } = req.params;
 
-  const conta = contas.find((conta) => conta.id == id);
+  const account = accounts.find((account) => account.id == id);
 
   const amount = req.body.amount;
-  const data = req.body.data;
-  const tipo = req.body.tipo;
+  const date = req.body.date;
+  const type = req.body.type;
 
-  if (conta.currentBalance > 0 && conta.currentBalance >= amount && amount != null) {
-    conta.currentBalance -= amount;
-    conta.arrayDeposit.push({
-      data: data,
-      valor: amount,
-      tipo: tipo,
+  if (account.currentBalance > 0 && account.currentBalance >= amount && amount != null) {
+    account.currentBalance -= amount;
+    account.arrayDeposit.push({
+      date: date,
+      value: amount,
+      type: type,
     });
   }
 
   if (!amount) return res.status(400).json();
 
-  if (!conta) return res.status(404).json({ resposta: "conta inexistente" });
+  if (!account) return res.status(404).json({ response: "Account not exist" });
 
-  if (amount > conta.currentBalance) {
-    conta.currentBalance = conta.currentBalance;
+  if (amount > account.currentBalance) {
+    account.currentBalance = account.currentBalance;
   }
-  if (conta.currentBalance == undefined) {
-    conta.currentBalance = 0;
+  if (account.currentBalance == undefined) {
+    account.currentBalance = 0;
   } else {
-    return res.status(200).json(conta);
+    return res.status(200).json(account);
   }
 });
 
 app.get("/accounts", function (req, res) {
-  if (contas.length <= 0) return res.status(200).json();
-  res.json(contas);
+  if (accounts.length <= 0) return res.status(200).json();
+  res.json(accounts);
 });
 
 app.get("/accounts/:id", function (req, res) {
   const { id } = req.params;
-  const conta = contas.find((conta) => conta.id == id);
+  const account = accounts.find((account) => account.id == id);
 
-  if (!conta) return res.status(404).json();
+  if (!account) return res.status(404).json();
 
-  return res.json(conta);
+  return res.json(account);
 });
 
 app.patch("/accounts/:id", function (req, res) {
   const { id } = req.params;
-  const conta = contas.find((conta) => conta.id == id);
-  console.log(contas);
+  const account = accounts.find((account) => account.id == id);
 
   let name = req.body.name;
-
   let overdraftProtection = req.body.overdraftProtection.toFixed(1);
 
-  if (!conta) return res.status(404).json({ resposta: "conta inexistente" });
+  if (!account) return res.status(404).json({ response: "Account not exist" });
 
   if (name != null) {
-    conta.name = name;
+    account.name = name;
   }
 
   if (overdraftProtection != null) {
-    conta.overdraftProtection = overdraftProtection;
+    account.overdraftProtection = overdraftProtection;
   }
 
   if (!name && !overdraftProtection) return res.status(400).json();
 
-  res.status(200).json(conta);
+  res.status(200).json(account);
 });
 
 app.delete("/accounts/:id", function (req, res) {
   const { id } = req.params;
-  const conta = contas.find((conta) => conta.id == id);
+  const account = accounts.find((account) => account.id == id);
 
-  if (conta.currentBalance > 0)
-    return res.status(400).json({ resposta: "Não é possível encerrar a conta" });
+  if (account.currentBalance > 0)
+    return res.status(400).json({ response: "Unable to close account" });
 
-  if (conta) {
-    const contaFiltrada = contas.filter((conta) => conta.id != id);
-    contas = contaFiltrada;
+  if (account) {
+    const filteredAccount = accounts.filter((account) => account.id != id);
+    accounts = filteredAccount;
 
-    return res.status(204).json({ resposta: "TUDO CERTO" });
+    return res.status(204).json({ response: "Account deleted" });
   }
 
-  if (!conta) return res.status(404).json({ resposta: "conta não encontrada" });
-  res.json(conta);
+  if (!account) return res.status(404).json({ response: "Account not found" });
+  res.json(account);
 });
 
 app.listen(4567, function () {
